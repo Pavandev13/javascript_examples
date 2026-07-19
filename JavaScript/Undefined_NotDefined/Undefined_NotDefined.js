30ca18bfb5b2d79da946952fc5bfeeb3ef273178
Pavan Kumar <vundelapavan84660@gmail.com>
2026-07-19 20:47:45 +0530

    hosting_scopchain_undefined changes

---
+++ JavaScript/Undefined_NotDefined/Undefined_NotDefined.js
@@ -0,0 +1,17 @@
+
+console.log(a)
+var a = 5;
+
+console.log(a)
+
+console.log(b)
+
+
+//Undefined is like a placeholder till a variable is not assigned a value
+//undefined!=not defined, Because undefined means variable is declared but no value is Assigned and not defined means Variable is not even declared.
+// JS is an Loosely Typed Language or Weakly Typed Language, You can Use same variable to store String, Integer, Boolean type Values in same.
+
+
+// a = undefined
+
+//Its not good practise to assign undefined to a variable
